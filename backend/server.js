fe1ae5f0d2ae4552adc1c01902df11ff736996cc
Michael Loughman <mcloughman@gmail.com>
2022-11-03 10:24:05 -0700

    Initial commit. Initiate  git in main folder directory. Create backend folder and import express and dotenv. Listen on 4000 and setup .env file. Test for functionality of initial simple GET route

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require("dotenv").config;
+const express = require("express");
+
+// express app
+const app = express();
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the app" });
+});
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log("Listening on port 4000");
+});
