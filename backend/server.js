3a71c410a1b207a16b3ce94b72a0cae0db5a656f
Michael Loughman <mcloughman@gmail.com>
2022-11-03 13:19:43 -0700

    separate routes away from server.js file. use express.json middleware in server.js. Setup database in mongodb atlas and test connection

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,32 @@
-require("dotenv").config;
+const path = require("path");
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts.js");
 
 // express app
 const app = express();
 
-// routes
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the app" });
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port 4000");
-});
+// routes --
+app.use(express.json()); // for sending data to the api - it will add it to the request object
+app.use("/api/workouts", workoutRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_UI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Listening on port 4000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
