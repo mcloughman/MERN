59c5a180bb491fa5e79c20d33e7c45674f972031
Michael Loughman <mcloughman@gmail.com>
2022-11-05 12:10:08 -0700

    Create models directory and setup workout schema. Also create controllers directory and write our backend functions there and then just export/import to routes. Routes will just evoke the proper function when a route is hit. We also add conditional in out controller file to check that a workout id is of correct mongoose type before we search the database for it.

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,20 +1,19 @@
 const express = require("express");
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+} = require("../controllers/workoutController");
 const router = express.Router();
 
 // GET All Workouts
-router.get("/", (req, res) => {
-  res.json({ msg: "GET all workouts" });
-});
+router.get("/", getWorkouts);
 
 // GET a single
-router.get("/:id", (req, res) => {
-  res.json({ msg: "GET a specific workout" });
-});
+router.get("/:id", getWorkout);
 
 // POST a new workout
-router.post("/", (req, res) => {
-  res.json({ msg: "POST a new workout" });
-});
+router.post("/", createWorkout);
 
 // DELETE a workout
 router.delete("/:id", (req, res) => {
